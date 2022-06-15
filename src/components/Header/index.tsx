21630f16d76f6e0d0af8880a2be0b7d0446243fb
felzika <fel_2@live.com>
2022-06-14 23:47:08 -0300

    Adiciona o component Banner (#6)
    Adicionado o componente Banner e retirado do componente Header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,16 +4,13 @@ import LogoImg from '../../assets/img/perfil.png';
 
 const Header: FC = () => {
   return (
-    <>
-      <div className="banner" />
-      <header className="header">
-        <figure>
-          <img src={LogoImg} alt="Minas Café logo" />
-        </figure>
-        <h1 className="header-title"> Minas Café </h1>
-        <h2 className="header-subtitle"> Cardápio </h2>
-      </header>
-    </>
+    <header className="header">
+      <figure>
+        <img src={LogoImg} alt="Minas Café logo" />
+      </figure>
+      <h1 className="header-title"> Minas Café </h1>
+      <h2 className="header-subtitle"> Cardápio </h2>
+    </header>
   );
 };
 
