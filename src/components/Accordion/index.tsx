d733b2bad18174783ae4529ed71144bc4072e8b9
felzika <fel_2@live.com>
2022-06-16 02:41:51 -0300

    Adiciona geração de dados do Accordion usando API
    Adicionado geração de dados do Accordion usando a API.
    Adicionado Accordion responsivo.
    Resolvido alguns Hotfix do review.

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 import api from '../../services/api';
 import './styles.css';
 
@@ -14,59 +14,72 @@ interface IProduct {
 }
 
 const Accordion: FC = () => {
+  const [categories, setCategories] = useState<ICategory[]>([]);
   const [products, setProducts] = useState<IProduct[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   useEffect(() => {
     async function getCategories() {
+      const response = await api.get('/categorias');
+
+      setCategories(response.data.data);
+    }
+    async function getProducts() {
       const response = await api.get('/produtos');
 
       setProducts(response.data.data);
     }
 
     getCategories();
+    getProducts();
   }, []);
+
+  const handleShowAccordion = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement>) => {
+      if (isOpen) {
+        event.currentTarget.classList.remove('accordion__button--is-open');
+        event.currentTarget.nextElementSibling?.classList.remove(
+          'accordion__content--is-open',
+        );
+      } else {
+        event.currentTarget.classList.add('accordion__button--is-open');
+        event.currentTarget.nextElementSibling?.classList.add(
+          'accordion__content--is-open',
+        );
+      }
+      setIsOpen(!isOpen);
+    },
+    [isOpen],
+  );
 
   return (
     <div className="accordion">
       <section className="accordion__inner">
-        <div className="accordion__item">
-          <button type="button" className="accordion__button">
-            <div className="accordion__icon">
-              <i className="fa-solid fa-mug-saucer" />
+        {categories.map((category: ICategory) => (
+          <div className="accordion__item" key={category.id}>
+            <button
+              type="button"
+              onClick={handleShowAccordion}
+              className="accordion__button"
+            >
+              <div className="accordion__icon">
+                <i className="fa-solid fa-mug-saucer" />
+              </div>
+              <div className="accordion__title">{category.name}</div>
+            </button>
+            <div className="accordion__content">
+              {products.map(
+                (product: IProduct) =>
+                  category.id === product.category.id && (
+                    <p className="product" key={product.id}>
+                      <span className="product__title">{product.name}</span>
+                      {/* <span className="product__price">R$ 2,50</span> */}
+                    </p>
+                  ),
+              )}
             </div>
-            <div className="accordion__title">Cafés</div>
-          </button>
-          <div className="accordion__content">
-            <p className="product">
-              <span className="product__title">Café coado</span>
-              <span className="product__price">R$ 2,50</span>
-            </p>
-            <p className="product">
-              <span className="product__title">Café coado com leite</span>
-              <span className="product__price">R$ 3,50</span>
-            </p>
-            <p className="product">
-              <span className="product__title">Café expresso</span>
-              <span className="product__price">R$ 5,00</span>
-            </p>
-            <p className="product">
-              <span className="product__title">Café expresso com leite</span>
-              <span className="product__price">R$ 6,00</span>
-            </p>
-            <p className="product">
-              <span className="product__title">Cappuccino</span>
-              <span className="product__price">R$ 6,00</span>
-            </p>
-            <p className="product">
-              <span className="product__title">Chocolate quente</span>
-              <span className="product__price">R$ 6,00</span>
-            </p>
-            <p className="product">
-              <span className="product__title">Chocolate frio</span>
-              <span className="product__price">R$ 6,00</span>
-            </p>
           </div>
-        </div>
+        ))}
       </section>
     </div>
   );
